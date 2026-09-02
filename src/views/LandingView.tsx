975aee85202652a9e6f078911d02ccc3df269f82
Luis Polo <guillepologarcia@gmail.com>
2026-09-02 09:38:06 -0500

    fix: elevate z-index of navbar so decorative background doesn't block click events

--- src/views/LandingView.tsx
+++ src/views/LandingView.tsx
@@ -8,7 +8,7 @@ export function LandingView({ onNavigate }: Props) {
   return (
     <div className="bg-white min-h-screen">
       {/* Navbar */}
-      <nav className="border-b border-gray-100">
+      <nav className="relative z-50 border-b border-gray-100 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16 items-center">
             <div className="flex items-center gap-2">
